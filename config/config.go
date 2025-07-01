21d2c0ed1bd9a359a6f9719bb59b30c15ed631dd
AswinPuthukaatil <aswin100396@gmail.com>
2025-07-01 11:53:28 +0530

    wrote basic configuration function using godotenv

--- config/config.go
+++ config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	MongoURI          string
+	MongoDB           string
+	ServerPort        string
+	JWTSecret         string
+	FirebaseCredFile  string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, reading env from system...")
+	}
+
+	config := &Config{
+		MongoURI:         os.Getenv("MONGO_URI"),
+		MongoDB:          os.Getenv("MONGO_DB"),
+		ServerPort:       os.Getenv("SERVER_PORT"),
+		JWTSecret:        os.Getenv("JWT_SECRET"),
+		FirebaseCredFile: os.Getenv("FIREBASE_CREDENTIALS"),
+	}
+
+	return config
+}
