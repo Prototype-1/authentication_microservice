ab217ed404580988e9a2e98a92bbd74b25e5817c
AswinPuthukaatil <aswin100396@gmail.com>
2025-07-03 22:49:38 +0530

    exposed a mock signup api for testing firebase

--- routes/router.go
+++ routes/router.go
@@ -1,1 +1,22 @@
 package routes
+
+import (
+	"github.com/Prototype-1/authentication_microservice/config"
+	"github.com/Prototype-1/authentication_microservice/internal/handler"
+	"github.com/gin-gonic/gin"
+	firebase "firebase.google.com/go/v4/auth"
+	"cloud.google.com/go/firestore"
+)
+
+func SetupRouter(cfg *config.Config, authClient *firebase.Client, firestoreClient *firestore.Client) *gin.Engine {
+	r := gin.Default()
+	userHandler := handler.NewUserHandler(authClient, firestoreClient)
+
+	api := r.Group("/api")
+	{
+		api.POST("/signup", userHandler.SignUp)
+
+	}
+
+	return r
+}
